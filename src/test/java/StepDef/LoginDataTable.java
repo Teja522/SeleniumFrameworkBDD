ad72fde4a3fe1b7fc9c1c63f44bac02bb77a66eb
steja <tejas522@gmail.com>
2025-03-20 00:09:11 +0530

    new

--- src/test/java/StepDef/LoginDataTable.java
+++ src/test/java/StepDef/LoginDataTable.java
@@ -52,7 +52,7 @@ public class LoginDataTable {
 
     @Then("User should be logged in successfully")
     public void user_should_be_logged_in_successfully() {
-        System.out.println("End");
+        System.out.println("Browser Closed");
         driver.quit();
     }
 }
